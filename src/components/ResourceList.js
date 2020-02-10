0cbf2d3bcd5331cd60d13e0cb1970f03a267017c
SorinGabriel02 <lupu_sg@yahoo.com>
2020-02-10 17:34:06 +0200

    first commit

---
+++ src/components/ResourceList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import useResources from "./useResources";
+
+const ResourseList = ({ resource }) => {
+  const resources = useResources(resource);
+
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourseList;
