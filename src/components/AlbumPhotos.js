8b085d413dd3daaa86a031271502a3cb3d029a52
SorinGabriel02 <lupu_sg@yahoo.com>
2020-05-09 12:09:35 +0300

    finishing AlbumPhotos

--- src/components/AlbumPhotos.js
+++ src/components/AlbumPhotos.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
+import axios from "axios";
 
 function AlbumPhotos() {
   const { userId, albumId } = useParams();
   const history = useHistory();
   const { authorName, albumTitle } = history.location.state;
+  const [photos, setPhotos] = useState([]);
+
+  async function getPhotosByAlbumId(albumId) {
+    const response = await axios
+      .get(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
+      .then((response) => response.data)
+      .catch((err) => console.error(err));
+    setPhotos(response);
+  }
+
+  useEffect(() => {
+    getPhotosByAlbumId(albumId);
+  }, [albumId]);
+
+  const photosJSX =
+    photos &&
+    photos.map(({ id, title, thumbnailUrl }) => {
+      return (
+        <li className="users" key={id}>
+          <figure>
+            <img style={{ borderRadius: 3 }} src={thumbnailUrl} alt={title} />
+            <figcaption>{title}</figcaption>
+          </figure>
+        </li>
+      );
+    });
 
   return (
     <section>
@@ -22,7 +49,7 @@ function AlbumPhotos() {
         </Link>
       </h1>
       <h2>Title: {albumTitle}</h2>
-      <ul></ul>
+      <ul style={{ display: "flex", flexWrap: "wrap" }}>{photosJSX}</ul>
     </section>
   );
 }
